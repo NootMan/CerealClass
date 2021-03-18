c38bf9c9080444760cef2afbaccfb1b2909f7c3c
Tristian Quijas <30303332+NootMan@users.noreply.github.com>
2021-03-18 10:22:03 -0700

    Add files via upload

---
+++ Runner.java
@@ -0,0 +1,8 @@
+public class test
+{
+    public static void main(String[] args)
+    {
+        Cereal cocoaPuffs = new Cereal("Cocoa Puffs");
+        System.out.println(cocoaPuffs.getName());
+    }    
+}
